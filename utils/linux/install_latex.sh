b447a02864684a242ecac209b7835cfd6b0733f0
d.bulgakov <bulgach09@gmail.com>
2024-05-05 00:46:56 +0300

    utils/linux: add install latex script

--- utils/linux/install_latex.sh
+++ utils/linux/install_latex.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Install base texlive packages
+sudo apt install texlive-base texlive-xetex texlive-lang-cyrillic
+sudo apt install texlive-latex-recommended
+
+# Install Times New Roman font
+sudo apt install ttf-mscorefonts-installer
+
+# Install xelatex and latexmk
+sudo apt install latexmk
+sudo apt install xelatex
+
+# Include Paths using .bashrc file
+echo "# Latex config" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/share/texlive" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/bin/xelatex" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/bin/latexmk" >> ~/.bashrc
 
